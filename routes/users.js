7bdd8f4e7cef41ca6cf706cb2e609a92f15ce499
Higor Bianchetti <higorbianchetti@gmail.com>
2020-08-08 16:05:54 -0300

    Add validations

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
 const { Router } = require('express');
-const { getUsers, createUser } = require('../controllers/users.js');
+const { check } = require('express-validator');
+
+const { getUsers, createUser } = require('../controllers/users');
+const { validator } = require('../middlewares/validator');
 
 const router = Router();
 
 router.get('/', getUsers);
 
-router.post('/', createUser);
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('password', 'Password is required').not().isEmpty(),
+    check('email', 'Email is required').isEmail(),
+    validator,
+  ],
+  createUser
+);
 
 module.exports = router;
